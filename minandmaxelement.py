18f2800733b869452774e7e4b1b8e9fa26734192
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-04 10:56:30 +0530

    Add files via upload

---
+++ minandmaxelement.py
@@ -0,0 +1,12 @@
+# Define an array
+a = [1, 423, 6, 46, 34, 23, 13, 53, 4]
+
+# Sort the array using the built-in sorted() function
+a_sorted = sorted(a)
+
+# Find the minimum and maximum values
+min_value = a_sorted[0]
+max_value = a_sorted[-1]
+
+# Print the results
+print(f"min-{min_value} max-{max_value}")
